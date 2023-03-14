5bdf3feb49435573ec6810311ee777c75764576c
SUPERcocoCats <86012304+SUPERcocoCats@users.noreply.github.com>
2023-03-14 14:51:01 -0700

    Update main.ts

--- main.ts
+++ main.ts
@@ -5,8 +5,8 @@ namespace tools {
           true, true, true, true, true,
            true, true, true, true, true,
             true, true, true, true, true];
-    //%blockid=copy
-    //%block="hi"
+    //%blockid=toolsCopy
+    //%block="copy screen"
     export function copy() {
         list = [led.point(0, 0), led.point(1, 0), led.point(2, 0), led.point(3, 0), led.point(4, 0),
                 led.point(0, 1), led.point(1, 1), led.point(2, 1), led.point(3, 1), led.point(4, 1),
@@ -14,7 +14,8 @@ namespace tools {
                 led.point(0, 3), led.point(1, 3), led.point(2, 3), led.point(3, 3), led.point(4, 3),
                 led.point(0, 4), led.point(1, 4), led.point(2, 4), led.point(3, 4), led.point(4, 4)];
     }
-
+    //%blockid=toolsPaste
+    //%block="paste screen"
     export function paste() {
         basic.clearScreen();
         for (let x = 0;x < 4; x++) {
