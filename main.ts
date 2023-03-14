cd9c88ce3b94c06d0de55c209bb8a0dffffdbdd7
SUPERcocoCats <86012304+SUPERcocoCats@users.noreply.github.com>
2023-03-14 14:48:39 -0700

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,16 +1,18 @@
-namespace led {
+namespace tools {
     let list = 
         [true, true, true, true,true,
          true, true, true, true, true,
           true, true, true, true, true,
            true, true, true, true, true,
             true, true, true, true, true];
+    //%blockid=copy
+    //%block="hi"
     export function copy() {
-        list = [point(0, 0), point(1, 0), point(2, 0), point(3, 0), point(4, 0),
-                point(0, 1), point(1, 1), point(2, 1), point(3, 1), point(4, 1),
-                point(0, 2), point(1, 2), point(2, 2), point(3, 2), point(4, 2),
-                point(0, 3), point(1, 3), point(2, 3), point(3, 3), point(4, 3),
-                point(0, 4), point(1, 4), point(2, 4), point(3, 4), point(4, 4)];
+        list = [led.point(0, 0), led.point(1, 0), led.point(2, 0), led.point(3, 0), led.point(4, 0),
+                led.point(0, 1), led.point(1, 1), led.point(2, 1), led.point(3, 1), led.point(4, 1),
+                led.point(0, 2), led.point(1, 2), led.point(2, 2), led.point(3, 2), led.point(4, 2),
+                led.point(0, 3), led.point(1, 3), led.point(2, 3), led.point(3, 3), led.point(4, 3),
+                led.point(0, 4), led.point(1, 4), led.point(2, 4), led.point(3, 4), led.point(4, 4)];
     }
 
     export function paste() {
