c66aabe55e6063daab2bd688b83a8eb163d91be3
SUPERcocoCats <86012304+SUPERcocoCats@users.noreply.github.com>
2023-03-15 11:01:56 -0700

    Update main.ts

--- main.ts
+++ main.ts
@@ -46,7 +46,7 @@ namespace tools {
             for (let i = 0; i < 26; i++) { if (subList[i]) { led.plot(Math.floor(i / 5),i % 5);}}
             }
             if (rot == 2) {
-                for (let i = 25; i > -1; i--) { if (subList[i]) { led.plot(i % 5, Math.floor(i / 5)); } }
+                for (let i = 0; i < 26; i++) { if (subList[i]) { led.plot((-i + 25) % 5, Math.floor((-i + 25) / 5)); } }
             }
         }
     export function flip() {
