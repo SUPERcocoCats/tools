6462d4aec147ffc9016e6d54372bfb81ae7d39e8
SUPERcocoCats <86012304+SUPERcocoCats@users.noreply.github.com>
2023-03-15 14:23:00 -0700

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 enum flipdir {
-    //% block="vertically"
+    //% block="2"
     vertically,
-    //% block="horizontally"
+    //% block="1"
     horizontally
 }
 
@@ -13,10 +13,10 @@ enum flipdir {
 namespace tools {
     let list = 
         [true, true, true, true,true,
-         true, true, true, true, true,
-          true, true, true, true, true,
-           true, true, true, true, true,
-            true, true, true, true, true];
+        true, true, true, true, true,
+        true, true, true, true, true,
+        true, true, true, true, true,
+        true, true, true, true, true];
     //%blockid=toolsCopy
     //%block="copy screen"
     export function copy() {
@@ -76,10 +76,15 @@ namespace tools {
         led.point(0, 2), led.point(1, 2), led.point(2, 2), led.point(3, 2), led.point(4, 2),
         led.point(0, 3), led.point(1, 3), led.point(2, 3), led.point(3, 3), led.point(4, 3),
         led.point(0, 4), led.point(1, 4), led.point(2, 4), led.point(3, 4), led.point(4, 4)];
-    
-        basic.clearScreen();
-        for (let m = 0; m < 26; m++) { if (subList[m]) { led.plot((-m + 24) % 5, Math.floor(m / 5)); } }
-    }
 
+        basic.clearScreen();
+        if (dir = 1) {
+        for (let m = 0; m < 26; m++) { if (subList[m]) { led.plot((-m + 24) % 5, Math.floor(m / 5)); } }
+        }
+        if (dir = 2) {
+            for (let m = 0; m < 26; m++) { if (subList[m]) { led.plot((-m + 24) % 5, Math.floor(m / 5)); } }
+        }
+    }
+    
 
     }
