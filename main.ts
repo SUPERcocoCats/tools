8a9fe7a4ee4296916b2a845e8f62cf801b562fa0
SUPERcocoCats <86012304+SUPERcocoCats@users.noreply.github.com>
2023-03-15 10:50:11 -0700

    Update main.ts

--- main.ts
+++ main.ts
@@ -42,7 +42,7 @@ namespace tools {
             
             //paste rotated
             basic.clearScreen();
-        for (let i = 0;i < 26; i++) {if(subList[i]){led.plot(i % 5,Math.floor(i/5));}}
+            for (let i = 0; i < 26; i++) { if (subList[i]) { led.plot(Math.floor(i / 5),i % 5);}}
         }
     export function flip() {
 
