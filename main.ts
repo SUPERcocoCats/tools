ba53857f48839e466ae4fc8a495809cbeea892ef
SUPERcocoCats <86012304+SUPERcocoCats@users.noreply.github.com>
2023-03-15 14:47:55 -0700

    Update main.ts

--- main.ts
+++ main.ts
@@ -81,7 +81,9 @@ namespace tools {
         if (dir = flipdir.vertically) {
         for (let m = 0; m < 26; m++) { if (subList[m]) { led.plot((-m + 24) % 5, Math.floor(m / 5)); } }
         }
-        
+        if (dir = flipdir.horizontally) {
+            for (let m = 0; m < 26; m++) { if (subList[m]) { led.plot(m % 5, Math.floor(m / 5)); } }
+        }
     }
     
 
