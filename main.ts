c3aa3532511b81877c634e719bd2d0785d9d001e
SUPERcocoCats <86012304+SUPERcocoCats@users.noreply.github.com>
2023-03-15 12:52:36 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -18,9 +18,7 @@ namespace tools {
     //%block="paste screen"
     export function paste() {
         basic.clearScreen();
-        for (let y = 0; y < 5; y++)
-        for (let x = 0;x < 5; x++) {
-            if(list[x + 5*y]) {led.plot(x, y);}
+        for (let i = 0; i < 26; i++) { if (list[i]) { led.plot(i % 5, Math.floor(i / 5)); } 
         }}
     let subList =
         [true, true, true, true, true,
@@ -28,6 +26,8 @@ namespace tools {
             true, true, true, true, true,
             true, true, true, true, true,
             true, true, true, true, true];
+
+        
         //%blockid=toolsrot
         //%block="Rotate $rot degrees"
         //%rot.min=0 rot.max=4
@@ -44,19 +44,27 @@ namespace tools {
 
             if (rot == 1) {
                 basic.clearScreen();
-                for (let i = 0; i < 26; i++) { if (subList[i]) { led.plot(Math.floor(i / 5), (-i + 24) % 5);}}
+                for (let j = 0; j < 26; j++) { if (subList[j]) { led.plot(Math.floor(j / 5), (-j + 24) % 5);}}
             }
             if (rot == 2) {
                 basic.clearScreen();
-                for (let i = 0; i < 26; i++) { if (subList[(-i + 24)]) { led.plot(i % 5, Math.floor(i / 5)); } }
+                for (let k = 0; k < 26; k++) { if (subList[(-k + 24)]) { led.plot(k % 5, Math.floor(k / 5)); } }
             }
             if (rot == 3) {
                 basic.clearScreen();
-                for (let i = 0; i < 26; i++) { if (subList[i]) { led.plot(Math.floor((-i + 24) / 5), i % 5); } }
+                for (let l = 0; l < 26; l++) { if (subList[l]) { led.plot(Math.floor((-l + 24) / 5), l % 5); } }
             }
         }
+    //%blockid="toolsflip"
     export function flip() {
-
+        subList = [led.point(0, 0), led.point(1, 0), led.point(2, 0), led.point(3, 0), led.point(4, 0),
+        led.point(0, 1), led.point(1, 1), led.point(2, 1), led.point(3, 1), led.point(4, 1),
+        led.point(0, 2), led.point(1, 2), led.point(2, 2), led.point(3, 2), led.point(4, 2),
+        led.point(0, 3), led.point(1, 3), led.point(2, 3), led.point(3, 3), led.point(4, 3),
+        led.point(0, 4), led.point(1, 4), led.point(2, 4), led.point(3, 4), led.point(4, 4)];
+    
+        basic.clearScreen();
+        for (let i = 0; i < 26; i++) { if (subList[i]) { led.plot((-i + 24) % 5, Math.floor(i / 5)); } }
     }
 
 
