503e5653ad0451897e4c0112d802f28d17e4ea8d
SUPERcocoCats <86012304+SUPERcocoCats@users.noreply.github.com>
2023-03-14 21:36:31 -0700

    Update main.ts

--- main.ts
+++ main.ts
@@ -22,5 +22,28 @@ namespace tools {
         for (let x = 0;x < 5; x++) {
             if(list[x + 5*y]) {led.plot(x, y);}
         }}
+    let subList =
+        [true, true, true, true, true,
+            true, true, true, true, true,
+            true, true, true, true, true,
+            true, true, true, true, true,
+            true, true, true, true, true];
+        //%blockid=toolsrot
+        //%block="Rotate"
+        export function Rotate() {
+            //copy the screen
+            subList = [led.point(0, 0), led.point(1, 0), led.point(2, 0), led.point(3, 0), led.point(4, 0),
+            led.point(0, 1), led.point(1, 1), led.point(2, 1), led.point(3, 1), led.point(4, 1),
+            led.point(0, 2), led.point(1, 2), led.point(2, 2), led.point(3, 2), led.point(4, 2),
+            led.point(0, 3), led.point(1, 3), led.point(2, 3), led.point(3, 3), led.point(4, 3),
+            led.point(0, 4), led.point(1, 4), led.point(2, 4), led.point(3, 4), led.point(4, 4)];
+            
+            //paste rotated
+            basic.clearScreen();
+            for (let y = 0; y < 5; y++)
+                for (let x = 0; x < 5; x++) {
+                    if (list[x + 5 * y]) { led.plot(y, x); }
+                }
+        }
     }
 
