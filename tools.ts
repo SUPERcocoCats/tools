036d5667079e8c11bc8ad0fc938154f2008f6a8b
SUPERcocoCats <86012304+SUPERcocoCats@users.noreply.github.com>
2023-03-16 10:51:52 -0700

    Update tools.ts

--- tools.ts
+++ tools.ts
@@ -4,7 +4,7 @@ enum flipdir {
     //% block="horizontally"
     horizontally
 }
-//%weight=100 color=#11a67e
+//%weight=100 color=#dec804
 namespace tools {
     let list =
         [true, true, true, true, true,
